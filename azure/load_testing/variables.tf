2706cf4a3ca93b4aad2a66b9a5a7931f74841393
Kostas <skenderidis@gmail.com>
2021-11-19 14:16:49 +0000

    commit

--- azure/load_testing/variables.tf
+++ azure/load_testing/variables.tf
@@ -14,9 +14,9 @@ variable location				{default = "eastus"}
 variable tag 					{default = "k.skenderidis@f5.com"}
 
 
-variable count_eastus			{default = 0}
+variable count_eastus			{default = 3}
 variable count_westus			{default = 0}
-variable count_uksouth			{default = 1}
+variable count_uksouth			{default = 0}
 variable count_eastasia			{default = 0}
 
 
